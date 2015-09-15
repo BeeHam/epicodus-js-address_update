20c0d917e6fe4ae12e6e3ed594cad84709bb4c2a
Brigham Thompson & Janice Dunn <student@epicodus.com>
2015-09-15 10:02:37 -0700

    add spec and code for Address constructor and fullAddress prototype

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,16 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
 $(document).ready(function() {
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-addresses">' +
@@ -46,11 +56,8 @@ $(document).ready(function() {
     });
 
 
-
-
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
-
+    // $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
